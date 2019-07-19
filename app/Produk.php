dd5a9cb1b141b06721552ba48846bec7e5f0ff82
aep <aepsaprul18@gmail.com>
2019-07-20 06:01:42 +0700

    update model produk

--- app/Produk.php
+++ app/Produk.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'nama', 
+        'kondisi_id',
+        'kategori_id',
+        'berat',
+        'merek',
+        'deskripsi',
+        'stok',
+        'harga',
+        'gambar1',
+        'gambar2',
+        'gambar3',
+        'terjual',
+        'dilihat'
+    ];
+
+    public function data_kondisi()
+    {
+        return $this->belongsTo('App\Kondisi', 'kondisi_id', 'id');
+    }
+    public function data_kategori()
+    {
+        return $this->belongsTo('App\Kategori', 'kategori_id', 'id');
+    }
 }
