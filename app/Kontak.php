d526292338383b92de4dea5333923065dae78690
aep <aepsaprul18@gmail.com>
2019-07-20 05:48:43 +0700

    update model kategori, kondisi, kontak, user

--- app/Kontak.php
+++ app/Kontak.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kontak extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'nama', 'keterangan', 'link', 'gambar'
+    ];
 }
