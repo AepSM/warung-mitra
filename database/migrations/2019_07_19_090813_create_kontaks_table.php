78eac0b999fbb535ee0a605d05d186858b17ce5d
aep <aepsaprul18@gmail.com>
2019-07-19 16:25:24 +0700

    make tabel database, make auth

--- database/migrations/2019_07_19_090813_create_kontaks_table.php
+++ database/migrations/2019_07_19_090813_create_kontaks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateKontaksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('kontaks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('level', 50)->nullable();
-            $table->rememberToken();
+            $table->string('nama', 50)->nullable();
+            $table->string('keterangan', 100)->nullable();
+            $table->string('link', 100)->nullable();
+            $table->string('gambar', 100)->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('kontaks');
     }
 }
