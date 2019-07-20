adf9a65f5346c09fd97e23ed8ed1299ac8fc0f01
aep <aepsaprul18@gmail.com>
2019-07-20 17:43:13 +0700

    login and logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Dashboard</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>WM | @yield('title')</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -15,16 +16,8 @@
         <!-- Theme style -->
         <link rel="stylesheet" href="{{ asset('adminlte/dist/css/AdminLTE.min.css') }}">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
-        folder instead of downloading all of them to reduce the load. -->
-        {{-- <link rel="stylesheet" href="{{ asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
-        <!-- jvectormap -->
-        <link rel="stylesheet" href="{{ asset('adminlte/bower_components/jvectormap/jquery-jvectormap.css') }}">
-        <!-- Date Picker -->
-        <link rel="stylesheet" href="{{ asset('adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
-        <!-- Daterange picker -->
-        <link rel="stylesheet" href="{{ asset('adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">
-        <!-- bootstrap wysihtml5 - text editor -->
-        <link rel="stylesheet" href="{{ asset('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}"> --}}
+            folder instead of downloading all of them to reduce the load. -->
+        <link rel="stylesheet" href="{{ asset('adminlte/dist/css/skins/_all-skins.min.css') }}">
 
         @yield('style')
 
@@ -35,70 +28,42 @@
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-        <!-- Google Font -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     </head>
-    @if (Auth::check())
-        <body class="hold-transition skin-blue sidebar-mini">
-            <div class="wrapper">
-                @include('layouts.topbar');
-                
-                @include('layouts.sidebar');
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
         
-                @yield('content')  
-                
-                <footer class="main-footer">
-                    <div class="pull-right hidden-xs">
-                    <b>Version</b> 2.4.0
-                    </div>
-                    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-                    reserved.
-                </footer>
-                <!-- /.control-sidebar -->
-                <!-- Add the sidebar's background. This div must be placed
-                    immediately after the control sidebar -->
-                <div class="control-sidebar-bg"></div>
-            </div>
-            <!-- ./wrapper -->
-    @else
-        <body class="hold-transition login-page">
-            @yield('content')
-    @endif    
+            @include('layouts.topbar')
+        
+            @include('layouts.sidebar')
 
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                
+                @yield('content')
+
+            </div>
+        <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b> 2.4.0
+                </div>
+                <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+                reserved.
+            </footer>
+            <div class="control-sidebar-bg"></div>
         <!-- jQuery 3 -->
         <script src="{{ asset('adminlte/bower_components/jquery/dist/jquery.min.js') }}"></script>
         <!-- jQuery UI 1.11.4 -->
-        {{-- <script src="{{ asset('adminlte/bower_components/jquery-ui/jquery-ui.min.js') }}"></script> --}}
+        <script src="{{ asset('adminlte/bower_components/jquery-ui/jquery-ui.min.js') }}"></script>
         <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-        {{-- <script>
-            $.widget.bridge('uibutton', $.ui.button);
-        </script> --}}
+        <script>
+        $.widget.bridge('uibutton', $.ui.button);
+        </script>
         <!-- Bootstrap 3.3.7 -->
         <script src="{{ asset('adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-        {{-- <!-- Sparkline -->
-        <script src="{{ asset('adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
-        <!-- jvectormap -->
-        <script src="{{ asset('adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ asset('adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-        <!-- jQuery Knob Chart -->
-        <script src="{{ asset('adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js') }}"></script>
-        <!-- daterangepicker -->
-        <script src="{{ asset('adminlte/bower_components/moment/min/moment.min.js') }}"></script>
-        <script src="{{ asset('adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
-        <!-- datepicker -->
-        <script src="{{ asset('adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
-        <!-- Bootstrap WYSIHTML5 -->
-        <script src="{{ asset('adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-        <!-- Slimscroll -->
-        <script src="{{ asset('adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
-        <!-- FastClick -->
-        <script src="{{ asset('adminlte/bower_components/fastclick/lib/fastclick.js') }}"></script>
         <!-- AdminLTE App -->
         <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="{{ asset('adminlte/dist/js/pages/dashboard.js') }}"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="{{ asset('adminlte/dist/js/demo.js') }}"></script> --}}
 
         @yield('script')
     </body>
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,10 +1,10 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>A</b>LT</span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Admin</b>LTE</span>
+    <a href="#" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>WM</b></span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>Warung Mitra</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -18,48 +18,35 @@
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                    <span class="hidden-xs">Alexander Pierce</span>
+                        <img src="{{ asset('adminlte/dist/img/profile/profile.png') }}" class="user-image" alt="User Image">
+                        <span class="hidden-xs">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu">
-                    <!-- User image -->
-                    <li class="user-header">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                        <p>
-                        Alexander Pierce - Web Developer
-                        <small>Member since Nov. 2012</small>
-                        </p>
-                    </li>
-                    <!-- Menu Body -->
-                    <li class="user-body">
-                        <div class="row">
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Followers</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Sales</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                            <a href="#">Friends</a>
-                        </div>
-                        </div>
-                        <!-- /.row -->
-                    </li>
-                    <!-- Menu Footer-->
-                    <li class="user-footer">
-                        <div class="pull-left">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        </div>
-                        <div class="pull-right">
-                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                        </div>
-                    </li>
+                        <!-- User image -->
+                        <li class="user-header">
+                                <img src="{{ asset('adminlte/dist/img/profile/profile.png') }}" class="img-circle" alt="User Image">
+                            <p>
+                                {{ Auth::user()->name }}
+                                <small>{{ Auth::user()->email }}</small>
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="{{ route('logout') }}"  class="btn btn-default btn-flat"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                     </ul>
-                </li>
-                <!-- Control Sidebar Toggle Button -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
                 </li>
             </ul>
         </div>
